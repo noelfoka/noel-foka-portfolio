602e4a8cd7f7aa226f58876652a8da2206238e10
noelfoka <nomgnenoel@gmail.com>
2023-06-03 13:41:46 +0100

    responsive navbar

--- frontend-app/src/component/Navbar/Navbar.jsx
+++ frontend-app/src/component/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { HiMenuAlt4, HIX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 import "./Navbar.scss";
 import images from "../../constants";
+import { useSyncExternalStore } from "react";
+import { useState } from "react";
 
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -19,6 +25,28 @@ function Navbar() {
           )
         )}
       </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HIX onClick={() => setToggle(false)} />
+            {["Home", "About", "Contact", "Work", "Skills", "Contact"].map(
+              (item) => (
+                <li key={item}>
+                  <div />
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              )
+            )}
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 }
