9dc035fae943744ca3a3b9980669c1174ec1321d
noelfoka <nomgnenoel@gmail.com>
2023-06-08 12:35:33 +0100

    add something

--- backend/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import about from './about'
-import brands from './brands'
-import testimonials from './testimonials'
-import contact from './contact'
-import experiences from './experiences'
-
-export const schemaTypes = [testimonials, about, brands, contact, experiences]
