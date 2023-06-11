10b2f25151d6e0dfea2f4dce981cb5594ac25446
noelfoka <nomgnenoel@gmail.com>
2023-06-11 08:48:53 +0100

    update all the project

--- frontend-app/src/component/index.js
+++ frontend-app/src/component/index.js
@@ -1,3 +1,9 @@
-export { default as Navbar } from "./Navbar/Navbar";
+/*export { default as Navbar } from "./Navbar/Navbar";
 export { default as NavigationDots } from "./NavigationDots";
-export { default as SocialMedia } from "./SocialMedia";
+export { default as SocialMedia } from "./SocialMedia";*/
+
+import NavigationDots from "./NavigationDots";
+import SocialMedia from "./SocialMedia";
+import Navbar from "./Navbar/Navbar";
+
+export { NavigationDots, SocialMedia, Navbar };
--- frontend-app/src/container/Footer/Footer.jsx
+++ frontend-app/src/container/Footer/Footer.jsx
@@ -47,7 +47,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="nomgnenoel@gmail.com" className="p-text">
+          <a href="mailto:nomgnenoel@gmail.com" className="p-text">
             nomgnenoel@gmail.com
           </a>
         </div>
