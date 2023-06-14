f4d2b9678d75d754e1eb0feff617d6eca14c09ff
noelfoka <nomgnenoel@gmail.com>
2023-06-14 17:14:00 +0100

    update images

--- frontend-app/src/container/Header/Header.jsx
+++ frontend-app/src/container/Header/Header.jsx
@@ -62,7 +62,7 @@ function Header() {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        {[images.react, images.redux, images.vue].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
