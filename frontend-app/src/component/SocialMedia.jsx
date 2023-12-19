1c7575f5aa483fd277a35d2ba8fb7d3a105a8101
noelfoka <nomgnenoel@gmail.com>
2023-12-19 06:28:19 +0100

    update

--- frontend-app/src/component/SocialMedia.jsx
+++ frontend-app/src/component/SocialMedia.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
+        <a href="https://github.com/noelfoka" target="_blank">
+          <IoLogoGithub />
+        </a>
       </div>
       <div>
-        <FaFacebookF />
+        <a href="https://www.linkedin.com/in/noelfoka/" taget="_blank">
+          <FaLinkedinIn />
+        </a>
       </div>
       <div>
-        <BsInstagram />
+        <a href="https://twitter.com/noelnomgne" target="_blank">
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a href="https://web.facebook.com/noel.foka?_rdc=2&_rdr" target="_blank">
+          <FaFacebookF />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.instagram.com/nomgnenoel/" target="_blank">
+          <BsInstagram />
+        </a>
       </div>
     </div>
   );
